d71cb27d0dd378008333a3b2e4e916ed49697098
Artem <artem.myronchenko@geozilla.com>
2026-09-08 02:15:05 +0300

    Rename app to Ferro (v1.5)
    Replace the Їжачок name with Ferro across the title, header,
    apple-mobile-web-app-title, and PWA manifest name/short_name.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ---------- Дані та збереження ----------
 
-const APP_VERSION = 'v1.4';
+const APP_VERSION = 'v1.5';
 
 const STORAGE_DISHES = 'ration.dishes.v1';
 const STORAGE_WEEKS = 'ration.weeks.v1';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ration-cache-v1.4';
+const CACHE_NAME = 'ration-cache-v1.5';
 const APP_SHELL = [
   './',
   './index.html',
